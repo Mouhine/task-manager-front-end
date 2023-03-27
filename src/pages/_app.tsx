bce0e4379386b81e95bcd6d59423a6df9741a9a4
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-03-27 22:25:53 +0100

    this message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { store } from "../redux/store";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../chakraTheme/Theme";
+import { SessionProvider } from "next-auth/react";
+import Layout from "@/components/Layout";
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <Provider store={store}>
+      <SessionProvider>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </SessionProvider>
+    </Provider>
+  );
 }
